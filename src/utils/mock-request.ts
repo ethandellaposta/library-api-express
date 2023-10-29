1ffe0af64a9a920fb8b430b85a7e903509d39ba8
Ethan Della Posta <ethanjdellaposta@gmail.com>
2023-10-28 23:32:13 -0400

    added better comments

--- src/utils/mock-request.ts
+++ src/utils/mock-request.ts
@@ -1,21 +1,4 @@
-import { BookCheckoutsService } from "../services/book-checkouts.service";
-import { BookCopiesService } from "../services/book-copies.service";
-import { BooksService } from "../services/books.service";
-import { UsersService } from "../services/users.service";
-
-function create_services() {
-  const books: BooksService = BooksService.get_instance();
-  const book_copies: BookCopiesService = BookCopiesService.get_instance();
-  const book_checkouts: BookCheckoutsService = BookCheckoutsService.get_instance();
-  const users: UsersService = UsersService.get_instance();
-
-  return {
-    books,
-    book_copies,
-    book_checkouts,
-    users
-  }
-}
+import { create_services } from "./create-services";
 
 // creates mocked values of controller function parameters
 export function mock_request({ params, body }: { params?: any; body?: any; services: any; }) {
